c9f58c9751656753fbd89d28416070d78feb1027
zekariasfe <zekariasfekeru@gmail.com>
2024-11-10 15:50:30 +0300

    props example

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,6 +7,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +18,7 @@ function App() {
         <Header />
         <MainContent />
         <Footer />  
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
