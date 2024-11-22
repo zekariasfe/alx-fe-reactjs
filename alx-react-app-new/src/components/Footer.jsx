c4083ca4915fa29f9397fa2f859b04d85f4e6425
zekariasfe <zekariasfekeru@gmail.com>
2024-11-22 10:37:08 +0300

    inline styles added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p style={{textAlign: "center", marginTop: "100px", opacity: "0.2", fontSize: "14px"}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+            <p style={{color: 'blue', fontSize: '24px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
