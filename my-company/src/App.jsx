870ef88b08ce431904bfcc026fbb6c6e4a77e28b
zekariasfe <zekariasfekeru@gmail.com>
2024-11-22 20:55:32 +0300

    my company

---
+++ my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contacts'
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
