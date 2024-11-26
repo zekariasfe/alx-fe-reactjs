cf58defea775174018450524feccf120adc00c62
zekariasfe <zekariasfekeru@gmail.com>
2024-11-26 14:03:09 +0300

    rr

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddRecipeForm />
+      <RecipeList />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
